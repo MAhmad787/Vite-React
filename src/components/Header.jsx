2e71db74af216408f6d149043447c282242b4f43
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-03-21 23:39:35 +0500

    Add Header.jsx & Update styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
-const header = () => {
+import PropTypes from 'prop-types';
+
+const header = ({ title }) => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 style={headingStyles}>{title}</h1>
     </div>
   );
 };
+header.defaultProps = {
+  title: 'Task Tracker',
+};
+header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
+// CSS Styles
+const headingStyles = {
+  color: 'white',
+  backgroundColor: 'red',
+  borderRadius: '10px',
+};
 export default header;
