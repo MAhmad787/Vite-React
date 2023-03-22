c1bbaa9a3a3295cbabda7af4561e529aead83eda
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-03-22 15:39:17 +0500

    Add button component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return <Header title="Task Tracker" />;
+  return (
+    <div className="container">
+      <Header title="Task Tracker" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,31 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
-const header = ({ title }) => {
+const Header = ({ title }) => {
+  const onClick = () => {
+    alert('You Clicked a Button');
+  };
   return (
-    <div>
-      <h1 style={headingStyles}>{title}</h1>
-    </div>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button text="Add" onClick={onClick} />
+    </header>
   );
 };
-header.defaultProps = {
+// Default Props
+Header.defaultProps = {
   title: 'Task Tracker',
 };
-header.propTypes = {
+// Types for the Props
+// Not Mandatory //
+Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
 // CSS Styles
-const headingStyles = {
-  color: 'white',
-  backgroundColor: 'red',
-  borderRadius: '10px',
-};
-export default header;
+// const headingStyles = {
+//   color: 'white',
+//   backgroundColor: 'red',
+//   borderRadius: '10px',
+// };
+export default Header;
